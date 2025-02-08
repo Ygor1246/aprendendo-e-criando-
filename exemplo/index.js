69b98c93feac89785e3efcb0126baa15131dfb0f
unknown <jhoranygor13@gmail.com>
2025-02-08 03:57:07 -0300

    aprendendo

---
+++ exemplo/index.js
@@ -0,0 +1,14 @@
+// esse e so um exemplo
+const num1 = 10;
+const num2 = parseInt('5'); // Já converte diretamente para número inteiro
+
+console.log(num1 + num2); // Soma os dois números
+console.log(typeof num2); // Mostra que "num2" agora é um número
+
+
+const num = 10;
+let nume = '1';
+
+console.log(num + Number(nume) ); // Soma os dois números
+console.log(typeof nume); // Mostra que e uma string de novo
+
---
+++ meu-repo-dev 📝/README.md
@@ -0,0 +1,10 @@
+# Meu Repositório de Códigos 🚀
+
+Este repositório contém meus projetos e experimentos em várias linguagens, como JavaScript, Python, e mais. Aqui vou guardar códigos que estou aprendendo e criando! 😃
+
+## Tecnologias Usadas 💻
+- JavaScript
+- Python
+- HTML & CSS
+- Node.js
+- Java
